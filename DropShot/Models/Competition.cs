2730c77cce53bbced95b0d4cf597f03efe2b1bc1
Alan Beattie <Alan.Beattie@xma.co.uk>
2026-03-26 21:05:04 +0000

    Competitions

---
+++ DropShot/Models/Competition.cs
@@ -0,0 +1,19 @@
+﻿namespace DropShot.Models
+{
+
+    public enum CompetitionFormat
+    {
+        Singles = 1,
+        Doubles = 2,
+        Team = 3
+    }
+
+    public class Competition
+    {
+        public int CompetitionID { get; set; }
+
+        public string CompetitionName { get; set; }
+
+        public CompetitionFormat CompetitionFormat { get; set; }
+    }
+}
